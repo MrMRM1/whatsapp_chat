8aac58c71e1ef300f8024b049378a1a1405e12af
MrMRM1 <9mehrabani@gmail.com>
2022-07-04 12:35:47 +0430

    feat: update

--- default_html.py
+++ default_html.py
@@ -14,34 +14,6 @@ head_html = ('''
     margin: 0;
     font: 14px/18px 'Open Sans',"Lucida Grande","Lucida Sans Unicode",Arial,Helvetica,Verdana,sans-serif;
 }
-strong {
-    font-weight: 700;
-}
-code, kbd, pre, samp {
-    font-family: Menlo,Monaco,Consolas,"Courier New",monospace;
-}
-code {
-    padding: 2px 4px;
-    font-size: 90%;
-    color: #c7254e;
-    background-color: #f9f2f4;
-    border-radius: 4px;
-}
-pre {
-    display: block;
-    margin: 0;
-    line-height: 1.42857143;
-    word-break: break-all;
-    word-wrap: break-word;
-    color: #333;
-    background-color: #f5f5f5;
-    border-radius: 4px;
-    overflow: auto;
-    padding: 3px;
-    border: 1px solid #eee;
-    max-height: none;
-    font-size: inherit;
-}
 .clearfix:after {
     content: " ";
     visibility: hidden;
@@ -63,9 +35,7 @@ pre {
     color: #168acd;
     text-decoration: none;
 }
-.page_wrap a:hover {
-    text-decoration: underline;
-}
+
 .page_header {
     /*position: fixed;*/
     z-index: 10;
@@ -90,12 +60,7 @@ pre {
 .details {
     color: #000000;
 }
-.details1 {
-    color: #000000;
-font-weight : 900;
-font-family: tahoma;
-direction: rtl;
-}
+
 .page_header .content .text {
     padding: 24px 24px 22px 24px;
     font-size: 22px;
@@ -112,19 +77,7 @@ text-align:center
     width: 480px;
     margin: 0 auto;
 }
-.page_about {
-    padding: 24px 24px;
-}
-.with_divider {
-    border-top: 1px solid #e3e6e8;
-}
-.userpic_link {
-    display: block;
-    text-decoration: none;
-}
-.userpic_link:hover {
-    text-decoration: none;
-}
+
 .userpic {
     display: block;
     border-radius: 50%;
@@ -137,134 +90,21 @@ text-align:center
     text-transform: uppercase;
     user-select: none;
 }
-.color_red,
-.userpic1,
-.media_call .fill,
-.media_file .fill,
-.media_live_location .fill {
-    background-color: #ff5555;
-}
+
 .color_green,
 .userpic2,
 .media_call.success .fill,
 .media_photo .fill {
     background-color: #64bf47;
 }
-.color_yellow,
-.userpic3,
-.media_venue .fill {
-    background-color: #ffab00;
-}
-.color_blue,
-.userpic4,
-.media_audio_file .fill,
-.media_voice_message .fill {
-    background-color: #4f9cd9;
-}
-.color_purple,
-.userpic5,
-.media_game .fill {
-    background-color: #9884e8;
-}
-.color_pink,
-.userpic6,
-.media_invoice .fill {
-    background-color: #e671a5;
-}
-.color_sea,
-.userpic7,
-.media_location .fill,
-.media_video .fill {
-    background-color: #47bcd1;
-}
-.color_orange,
-.userpic8,
-.media_contact .fill {
-    background-color: #ff8c44;
-}
-.personal_info {
-    padding: 24px;
-}
-.personal_info .userpic .initials {
-    font-size: 30px;
-}
-.personal_info .rows {
-    float: left;
-    padding-right: 24px;
-}
-.personal_info .names {
-    width: 164px;
-}
-.personal_info .info {
-    width: 124px;
-}
-.personal_info .bio {
-    width: 400px;
-}
-.personal_info .row {
-    padding-bottom: 16px;
-}
+
+
 a.block_link {
     display: block;
     text-decoration: none !important;
     border-radius: 4px;
 }
-a.block_link:hover {
-    text-decoration: none !important;
-    background-color: #f5f7f8;
-}
-.sections {
-    padding: 11px 0;
-}
-.section {
-    height: 48px;
-    background-position: 24px 12px;
-    background-repeat: no-repeat;
-    background-size: 24px 24px;
-}
-.section .counter {
-    float: right;
-    padding: 14px 24px 0;
-    font-size: 15px;
-}
-.section .label {
-    padding: 15px 0 0 82px;
-    font-size: 15px;
-}
-.list_page .page_about {
-    padding: 16px 24px 0;
-    font-size: 11px;
-color: #000000;
-font-weight : 900;
-font-family: tahoma;
-text-align: center;
-}
-.list_page .entry_list {
-    padding: 16px 0;
-}
-.list_page .entry {
-    padding: 10px 16px;
-}
-.list_page .entry .userpic .initials {
-    font-size: 18px;
-}
-.list_page .entry .body {
-    margin-left: 66px;
-}
-.list_page .entry .name {
-    padding: 4px 0 2px;
-    font-size: 14px;
-}
-.list_page .entry .subname {
-    padding-top: 4px;
-}
-.list_page .entry .details_entry {
-    padding-top: 4px;
-}
-.list_page .entry .info {
-    font-size: 11px;
-    padding-top: 5px;
-}
+
 .history {
     padding: 16px 0;
 }
@@ -272,24 +112,12 @@ text-align: center;
     margin: 0 -10px;
     transition: background-color 2.0s ease;
 }
-div.selected {
-    background-color: rgba(242,246,250,255);
-    transition: background-color 0.5s ease;
-}
+
 .service {
     padding: 10px 24px;
 }
 .service .body {
     text-align: center;
-}
-.service .userpic_wrap {
-    padding-top: 10px;
-}
-.service .userpic {
-    margin: 0 auto;
-}
-.service .userpic .initials {
-    font-size: 24px;
 }
 .message .userpic .initials {
     font-size: 16px;
@@ -350,71 +178,73 @@ Whatsapp chat
 ''')
 
 
-def massage_html(user, data, massage_text):
-    return ('''
+def message_html(user, datatime, time, massage_text):
+    return ('''<div class="message default clearfix">
 
-                         <div class="message default clearfix" id="message13085">
+      <div class="pull_left userpic_wrap">
 
-                      <div class="pull_left userpic_wrap">
+       <div class="userpic userpic2" style="width: 42px; height: 42px">
 
-                       <div class="userpic userpic2" style="width: 42px; height: 42px">
+        <div class="initials" style="line-height: 42px">
+{}
+        </div>
 
-                        <div class="initials" style="line-height: 42px">
-                {}
-                        </div>
+       </div>
 
-                       </div>
+      </div>
 
-                      </div>
+      <div class="body">
 
-                      <div class="body">
+       <div class="pull_right date details" title="{}">
+{}
+       </div>
 
-                       <div class="pull_right date details" title="{}">
-                {}
-                       </div>
+       <div class="from_name">
+{}
+       </div>
 
-                       <div class="from_name">
-                {}
-                       </div>
+       <div class="text">
+{}
+       </div>
 
-                       <div class="text">
-                {}
-                       </div>
+      </div>
 
-                      </div>
-
-                     </div>
-
-                        '''.format(user, data[0], data[1], data[3], massage_text))
+     </div>'''.format(user[0:2], datatime, time, user, massage_text))
 
 
-def continue_massage(massage_text):
-    return '''
-                                     <div class="message default clearfix" id="message13085">
+def continue_message(massage_text):
+    return '''<div class="message default clearfix joined">
 
-                      <div class="body">
+      <div class="body">
 
-                       <div class="text">
-                {}
-                       </div>
-    
-                      </div>
-    
-                     </div>
-                                    '''.format(massage_text)
+       <div class="pull_right date details" title="">
+       </div>
+
+       
+
+       <div class="text">
+{}
+       </div>
+
+      </div>
+
+     </div>'''.format(massage_text)
+
+
+def message_service(message):
+    return '''<div class="message service">
+
+      <div class="body details">
+{}
+      </div>
+
+     </div>'''.format(message)
 
 
 def footer_html():
     return '''
 
 </div>
-   <a class="content block_link" href="http://mrmrm.ir/" onclick="return GoBack(this)">
-
-     <div class="text bold">
-by Mohammad Reza Mehrabany
-     </div>
-
-    </a>
    </div>
 
   </div>
@@ -422,7 +252,4 @@ by Mohammad Reza Mehrabany
  </body>
 
 </html>
-
-
-
 '''
--- main.py
+++ main.py
@@ -1,79 +1,61 @@
 import codecs
 import re
-from tqdm import trange
-from colorama import init, Fore, Style
+import sys
+
 from default_html import *
-init()
-print(Style.BRIGHT+Fore.GREEN+"""
-                    chate whatsapp         
 
-            baraye dashtane khorogi behtar
-
-            hatman file rahnama ro bekhonin
-
-
-"""+Fore.MAGENTA)
-
-
-input1 = input("    name file chate whatsapp ro bedone .txt vared konin: ")+".txt"
-input2 = input1[:-4]+".html"
+input1 = sys.argv[1]
+output = input1[:-4] + ".html"
 
 
 def open_file(file_name, typ):
     return codecs.open(file_name, typ, encoding="utf-8")
 
 
-file_input = open_file(input1, "r")
-file_output = open_file(input2, "w")
-file_output.write(head_html)
-file_output.close()
-file_output = open_file(input2, 'a')
+def progressbar(it, prefix="", size=60, out=sys.stdout):
+    count = len(it)
 
-f_read = file_input.readlines()
-name = f_read[2].split()[3]
+    def show(j):
+        x = int(size * j / count)
+        print(f"{prefix}[{u'█' * x}{('.' * (size - x))}] {j}/{count}", end='\r', file=out, flush=True)
 
-m = 2
-user1 = ''
-user2 = ''
-while 1:
-    p = f_read[m].split()
-    m += 1
-    if name == p[3]:
-        user1 = p[3][0]
-    elif name != p[3]:
-        user2 = p[3][0]
-        break
+    show(0)
+    for i, item in enumerate(it):
+        yield item
+        show(i + 1)
+    print("\n", flush=True, file=out)
 
 
-print()
-bar = trange(len(f_read)-1, bar_format="{l_bar}{bar}{n_fmt}/{total_fmt} [{elapsed}]")
-for i in bar:
-    data = f_read[i].split()
-    massage = ' '.join(data[4:])
-    if data != []:
-        if len(data) > 1:
-            c6 = re.findall("\d{2}:\d{2}", data[1])
-            if c6 != []:
-                if data[3][0] == user1:
-                    file_output.write(massage_html(user1, data, massage))
-                else:
-                    file_output.write(massage_html(user2, data, massage))
-            else:
-                file_output.write(continue_massage(' '.join(data[0:])))
+with open_file(output, "w") as f:
+    f.write(head_html)
+
+with open_file(input1, "r") as f:
+    f_read = f.readlines().copy()
+
+file_output = open_file(output, 'a')
+
+for i in progressbar(range(len(f_read)-1)):
+    data = f_read[i]
+    if data:
+        try:
+            time = re.findall(r"\d{2}:\d{2}", data)[0]
+            datatime = re.findall(r'\d{2}.\d{2}.\d{2}', data)[0]
+        except:
+            file_output.write(continue_message(data))
+            continue
+
+        try:
+            user = re.findall(r'\d{2}.-.*: ', data)[0][5:][:-1]
+        except:
+            file_output.write(message_service(data))
+            continue
+
+        massage = re.findall(r': .*', data)[0][1:]
+
+        if user:
+            file_output.write(message_html(user, datatime, time, massage))
         else:
-            file_output.write(continue_massage(' '.join(data[0:])))
+            file_output.write(continue_message(data))
 
-
-print(Fore.YELLOW+"""
-            --------------END!--------------
-            ---                          ---
-            ---by Mohammad Reza Mehrabany---
-            ---                          ---
-            ---     http://Mrmrm.ir      ---
-            --------------------------------
-""")
-
-
-file_input.close()
 file_output.close()
-input()
+
